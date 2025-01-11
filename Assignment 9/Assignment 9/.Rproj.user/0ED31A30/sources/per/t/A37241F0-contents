98d75a042074b4b2c61e0f393d8772635fdee80c
stavlipitz <stavlipitz@mail.tau.ac.il>
2025-01-11 12:35:54 +0200

    Ready code for assignment and readme file

--- Assignment 9/Assignment 9/.Rproj.user/0ED31A30/sources/per/t/A37241F0-contents
+++ Assignment 9/Assignment 9/.Rproj.user/0ED31A30/sources/per/t/A37241F0-contents
@@ -3,15 +3,17 @@ library(dplyr)
 
 #### config ----
 set.seed(123)
+N = 120
 df = data.frame(
-  subject    = seq(from = 1, to = 120, by = 1),
-  gender     = sample(x = c("male", "female"), size = 120, replace = T),
-  rt         = rnorm(n = 120, mean = 3100, sd = 1450),
-  depression = runif(n = 120, min = 0, max = 100),
-  sleep_duration = rnorm(n = 120, mean = 7, sd = 2.5)
+  subject    = seq(from = 1, to = N, by = 1),
+  gender     = sample(x = c("male", "female"), size = N, replace = T),
+  age        = runif(n = N, min = 18, max = 50),
+  rt         = rnorm(n = N, mean = 3100, sd = 1450),
+  depression = runif(n = N, min = 0, max = 100),
+  sleep_duration = rnorm(n = N, mean = 7, sd = 2.5)
   )
 
 source("functions.R")
-summarise_df(df)
+results = summarise_df(df, 50, 100)
 
 
--- Assignment 9/Assignment 9/functions.R
+++ Assignment 9/Assignment 9/functions.R
@@ -1,19 +1,27 @@
-summarise_df <- function(df) {
+summarise_df <- function(df, subject_start, subject_end) {
+  if (nrow(df) < 10) {
+    print("data is too short")
+    return(NULL)
+  }
+  else{
+  # Turn rows to NA if subject is outside the range
+  df[df$subject < subject_start | df$subject > subject_end, ] = NA
+  
   # Apply function to each column in the data frame
   results <- lapply(df, function(col) {
+    # For character columns: return table of unique values
     if (class(col) == "character") {
-      # For character columns: return table of unique values
       obs_number <- table(col)
-    return(list(obs_number))
-    } 
-    else {
+      return(list(obs_number))
+    } else {
       # For numeric or other columns: return mean, min, and max
       mean_col <- mean(col, na.rm = TRUE)
-      min_col <- min(col, na.rm = TRUE)
-      max_col <- max(col, na.rm = TRUE)
+      min_col  <- min(col, na.rm = TRUE)
+      max_col  <- max(col, na.rm = TRUE)
       return(list(mean = mean_col, min = min_col, max = max_col))
     }
-  })
+  })}
+  
+  # Combine results into a data frame
   return(as.data.frame(results))
 }
-
